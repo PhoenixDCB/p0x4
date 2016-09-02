a226df8a6e0fcd7b7fef847c7170df12b1fd427f
Guillermo Cuesta Boluda <gcuestab@everis.com>
2016-09-02 14:46:15 +0200

    1. I have added the player score fragment with a recycler view (WIP)

---
+++ app/src/main/java/com/glacial/p0x4/fragments/PlayersScoreFragment.java
@@ -0,0 +1,53 @@
+package com.glacial.p0x4.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.glacial.p0x4.R;
+import com.glacial.p0x4.adapters.PlayersScoreAdapter;
+import com.glacial.p0x4.core.Game;
+import com.glacial.p0x4.general.Constants;
+
+/**
+ * Created by gcuestab on 2/9/16.
+ */
+public class PlayersScoreFragment extends Fragment {
+
+    public static final String TAG = PlayersScoreFragment.class.getName();
+
+    private RecyclerView rvPlayers;
+
+    public static PlayersScoreFragment newInstance(Game game) {
+        PlayersScoreFragment f = new PlayersScoreFragment();
+
+        Bundle b = new Bundle();
+        b.putSerializable(Constants.GAME, game);
+        f.setArguments(b);
+
+        return f;
+    }
+
+    public PlayersScoreFragment() {
+        super();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_players_score, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        PlayersScoreAdapter playersScoreAdapter = new PlayersScoreAdapter();
+
+        rvPlayers = (RecyclerView) getActivity().findViewById(R.id.rvPlayers);
+        rvPlayers.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvPlayers.setAdapter(playersScoreAdapter);
+    }
+}
--- app/src/main/java/com/glacial/p0x4/general/UtilsFragments.java
+++ app/src/main/java/com/glacial/p0x4/general/UtilsFragments.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import com.glacial.p0x4.R;
 import com.glacial.p0x4.core.Game;
 import com.glacial.p0x4.fragments.AddPlayersFragment;
+import com.glacial.p0x4.fragments.PlayersScoreFragment;
 
 /**
  * Created by dacuesta on 1/9/16.
@@ -24,11 +25,21 @@ public class UtilsFragments {
                 .commit();
     }
 
-    public static void goNext(FragmentManager fManager) {
-        //TODO: make the method. This is a dummy version. You r right man!
-        state = State.score;
-        fManager.beginTransaction()
-                .replace(R.id.rlContent, AddPlayersFragment.newInstance(null), AddPlayersFragment.TAG)
-                .commit();
+    public static void goNext(FragmentManager fManager, Game game) {
+        if (state.equals(State.create)) {
+            state = State.score;
+            fManager.beginTransaction()
+                    .replace(R.id.rlContent, PlayersScoreFragment.newInstance(game), PlayersScoreFragment.TAG)
+                    .commit();
+        }
+        else if (state.equals(State.score)) {
+            state = State.bet;
+        }
+        else if (state.equals(State.bet)) {
+            state = State.result;
+        }
+        else if (state.equals(State.result)) {
+            state = State.score;
+        }
     }
 }
