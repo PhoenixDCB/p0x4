15396ac170e17986ca5b61d466adbdc38d788541
dacuesta <phoe88@gmail.com>
2016-09-02 14:00:19 +0200

    MERGE

--- app/src/main/java/com/glacial/p0x4/fragments/AddPlayersFragment.java
+++ app/src/main/java/com/glacial/p0x4/fragments/AddPlayersFragment.java
@@ -3,6 +3,8 @@ package com.glacial.p0x4.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
