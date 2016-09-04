d66c2f2a43dcb32e8b76328f552308e55340f267
Guillermo Cuesta Boluda <gcuestab@everis.com>
2016-09-04 11:52:08 +0200

    1. The players introduced in the add players screen are shown in the player scores screen.

--- app/src/main/java/com/glacial/p0x4/adapters/PlayersScoreAdapter.java
+++ app/src/main/java/com/glacial/p0x4/adapters/PlayersScoreAdapter.java
@@ -4,9 +4,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.glacial.p0x4.R;
 import com.glacial.p0x4.core.Game;
+import com.glacial.p0x4.core.Player;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by dacuesta on 1/9/16.
@@ -22,24 +26,34 @@ public class PlayersScoreAdapter extends RecyclerView.Adapter<PlayersScoreAdapte
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_player_score, parent, false);
-        return new ViewHolder(v, game);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        if (position < game.getNumPlayers()) {
+            Player player = game.getPlayer(position);
+            if (player != null) {
+                holder.tvName.setText(player.getName());
+                holder.tvScore.setText(Integer.toString(player.getScore()));
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-//        return game.getlPlayer().size();
-        return 6;
+        return game.getNumPlayers();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvName;
+        private TextView tvScore;
 
-        public ViewHolder(View itemView, Game game) {
+        public ViewHolder(View itemView) {
             super(itemView);
+
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            tvScore = (TextView) itemView.findViewById(R.id.tvScore);
         }
     }
 
